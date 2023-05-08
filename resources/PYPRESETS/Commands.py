b81cb31b180732b01fd8aff5442ddee2e26cda2c
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-05-08 20:18:56 +1000

    Create Commands.py

---
+++ resources/PYPRESETS/Commands.py
@@ -0,0 +1,12 @@
+Class PlayerBase():
+  def __init__(self):
+   
+ 
+Class CommandUser(PlayerBase):
+  def help(self):
+    print('Your List of Commands are:')
+    for in Commands.keys():
+      print(keys)
+ Commands = {
+   'help' : CommandUser.help,
+ }
