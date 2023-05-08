afd0af653fbb96876c9e994fccb16ba982a40991
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-05-08 20:27:54 +1000

    Update main.php

--- src/PPC/main.php
+++ src/PPC/main.php
@@ -3,4 +3,5 @@
 namespace skyss0fly/PPC;
 
 class Main: {
-stuff coming soon
+public function onLoad(): void{
+$this->getLogger()->info("Python Compiler Loaded, if you or your player has the permission node " ..TEXTCOLOR:GOLD .. "PPC.use" ..TEXTFORMAT:RESET.. ", you will have access to " ..TEXTCOLOR:PURPLE.. "/PPC");
